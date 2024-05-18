c4620cfee4aa99427afc2bd63e756d585e5fa92a
hiromaru05s <maruru836818@gmail.com>
2024-05-18 04:58:26 -0400

    last commit! built all the UIs before users log-in so, the rest of work will be back-end. Validation in sign-in and sign-up UI could be done in this part, but i can't work on the auth and database stuff atm, so it will leave it and resume building this later.. :(

--- app/Notes/page.tsx
+++ app/Notes/page.tsx
@@ -1,9 +1,32 @@
 import Link from "next/link";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
+import { Button } from "@/components/ui/button"
 
-export default function About() {
+export default function Notes() {
   return (
     <div>
-      <div>Hello About!</div>
+      <div className="flex flex-col items-center justify-between text-lg pt-32 pb-16">Sign in first to see your notes!</div>
+      <div>
+        <div className="flex flex-col items-center justify-between ">
+        <Link
+        
+        href="/sign-in"
+        >
+       <Button className="cursor-customHover cursor-customHoverClick">
+        
+        Sign In
+        
+      </Button>
+      </Link>
+      </div>
+      </div>
     </div>
   );
 }
+
+
--- app/Scraps/page.tsx
+++ app/Scraps/page.tsx
@@ -5,11 +5,24 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion"
+import { Button } from "@/components/ui/button"
 
-export default function Blog() {
+export default function Scraps() {
   return (
     <div>
-      <div>Hello Blog!</div>
+      <div className="flex flex-col items-center justify-between text-lg pt-32 pb-16">Sign in first to throw your scraps!</div>
+      <div>
+        <div className="flex flex-col items-center justify-between">
+        <Link
+        
+        href="/sign-in"
+        >
+      <Button className="cursor-customHover cursor-customHoverClick">
+        Sign In
+      </Button>
+      </Link>
+      </div>
+      </div>
     </div>
   );
 }
